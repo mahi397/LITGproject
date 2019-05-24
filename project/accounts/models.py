e60c728a9d07d9e8fe1d101b31562843a44ed1db
Mahima <mahimasachdeva223@gmail.com>
2019-05-24 07:31:43 +0530

    created models(basic structure) in models.py  - User, Mood, Activity.

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    userid = models.AutoField(primary_key = True)
+    username = models.CharField(max_length = 20)
+    password = models.CharField(max_length = 20)
+
+
+class Mood(models.Model):
+    userid = models.IntegerField(primary_key = True)
+    timestamp = models.DateTimeField(primary_key = True)
+    mood_entry = models.CharField()
+
+
+class Acitivity(models.Model):
+    userid = models.IntegerField(primary_key = True)
+    timestamp = models.DateTimeField(primary_key = True)
+    activity = models.CharField()
