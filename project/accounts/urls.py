3e536c135d4c400e395848efd7f57a8e430f13ad
Mahima <mahimasachdeva223@gmail.com>
2019-06-08 19:44:49 +0530

    Created forms for taking user input for user's goals, mood and activities. The goal-form is accessible at /accounts/setgoals/, mood-form at /accounts/mymood, activity-form at /accounts/myactivities/. I started by creating forms.py in accounts directory and creating 3 form classes for goals, mood and activity. Added a function(validator) to restrict goal count to 3. Then I created views for each of the 3 forms. The values obtained from the forms have been caught into variables but not stored into databases yet. The goal form is accessed first. It redirects to mood form which in turn redirects to activity form. The activity form redirects to a page 'tracks for you' but this action throws an error. I've also added respective urls for the views in urls.py under accounts directory.

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name = 'signup'),
+    path('setgoals/', views.askGoals, name = 'setgoals'),
+    path('mymood/', views.askMood, name = 'mymood'),
+    path('myactivities/', views.askActivity, name = 'myactivities'),
 ]
--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,6 +1,11 @@
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+
+from .forms import MoodForm
+from .forms import ActivityForm
+from .forms import GoalForm
 
 def signup(request):
     if request.method == 'POST':
@@ -9,9 +14,48 @@ def signup(request):
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+            user = authenticate(username = username, password = raw_password)
             login(request, user)
             return redirect('login')
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
+
+def askGoals(request):
+    if request.method == 'POST':
+        # POST, generate form with data from the request
+        form = GoalForm(request.POST)
+        # check if it's valid:
+        if form.is_valid():
+            # process data, insert into DB, generate email,etc
+            goals = form.cleaned_data['goals']
+            
+            # redirect to a new url:    
+            return HttpResponseRedirect('/accounts/mymood')
+
+    else:
+        # GET, generate blank form
+        form = GoalForm()
+    return render(request, 'goalform.html', {'form': form})
+
+def askMood(request):
+    if request.method == 'POST':
+        form = MoodForm(request.POST)
+        if form.is_valid():
+            mood = form.cleaned_data['mood']
+            #store in db?
+            return HttpResponseRedirect('/accounts/myactivities')
+    else:
+        form = MoodForm()
+    return render(request, 'moodform.html', {'form': form})
+
+def askActivity(request):
+    if request.method == 'POST':
+        form = ActivityForm(request.POST)
+        if form.is_valid():
+            activity = form.cleaned_data['activity']
+            #store in db
+            return HttpResponseRedirect('/tracksforyou.html') #--error.
+    else:
+        form = ActivityForm()
+    return render(request, 'activityform.html', {'form': form})
